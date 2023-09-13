ce2686b712391778ab9cb5101cd56a3d7e5aa848
Miriam Fraga Rodríguez <fraga@MacBook-Pro-de-Miriam.local>
2023-09-13 18:11:35 +0200

    new logic

--- src/components/ProjectsComponent/Project.js
+++ src/components/ProjectsComponent/Project.js
@@ -22,7 +22,7 @@ const Project = [
             vercel: "https://pokeapi-seven-rose.vercel.app/",
             gitHub: "https://github.com/miriamfraga/pokeapi",
        }],
-        description: "", 
+        description: "The PokeAPI was the first project created as part of the bootcamp training. It was created using only JavaScript. It includes a simple name search and a series of buttons to search by types.", 
         },
     {
     projectName: "Bonsai",
@@ -51,7 +51,7 @@ const Project = [
         vercel: "https://shop-9c0ls0bmf-miriamfraga.vercel.app/",
         gitHub: "https://github.com/miriamfraga/shop",
 }],
-    description: "Bonsai es un proyecto realizado con angular, en el que se encuentra incluido un archivo json simulando un falso back, en el que he alojado una 'base de datos' para poder ir generando nuevo contenido que el usuario va creando desde la página de gestión."
+    description: "Bonsai is a project created with Angular, in which a JSON file simulating a fake backend is included. In this file, I have hosted a 'database' to generate new content that the user creates from the management page."
     },
     {
     projectName: "FilmsHub",
@@ -81,7 +81,7 @@ const Project = [
         vercel: "https://filmshub.vercel.app/",
         gitHub: "https://github.com/miriamfraga/proyectoreact" 
          }],
-    description: ""
+    description: "FilmsHub is the first React project that also includes Redux. It makes an API call to fetch different movie categories, along with all the available information. It contains a component that allows you to explore more detailed information about each movie. You can add movies you like to watch later."
     },
     {
     projectName: "Feminink",
@@ -111,7 +111,7 @@ const Project = [
      vercel: "https://feminink-g03tro5l7-feminink.vercel.app/",
      gitHub: "https://github.com/Feminink/Feminink"
     }],
-    description: ""
+    description: "Feminink is the final group project of the bootcamp. It is an extensive project with Redux and several libraries injected, not only for styles. It includes different forms for login and sign-in, a user profile page, and the ability to access as a tattoo artist or as a client. It also has a JSON file hosted to store user data when they sign up. It features a trivia game to earn discounts on tattoos."
     },
     {
         projectName: "Portfolio",
@@ -137,7 +137,7 @@ const Project = [
          vercel: "https://portfolio-two-ebon-58.vercel.app/",
          gitHub: "https://github.com/miriamfraga/Portfolio.git"
         }],
-        description: ""
+        description: "Portfolio is a simple project created with React, in which I have included a fun Medusa canvas that follows the cursor as you navigate through the project"
         }
 ]
 export default Project
--- src/components/ProjectsComponent/ProjectsComponent.jsx
+++ src/components/ProjectsComponent/ProjectsComponent.jsx
@@ -1,62 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './ProjectsComponent.scss';
 import { Link } from 'react-router-dom';
-//IMPORT PROJECT
 import Project from './Project';
-//IMPORT HASHSCROLL
-//IMPORT FONTAWESOME
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faLink} from '@fortawesome/free-solid-svg-icons';
-import {faGithub} from '@fortawesome/free-brands-svg-icons';
+import { faLink } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
+const ProjectsComponent = () => {
+  const [projectStates, setProjectStates] = useState([]);
+  const [hidden, setHidden] = useState([]);
+  
+  const changeVisibility = (index) => {
+    const newHidden = [...hidden];
+    newHidden[index] = !newHidden[index];
+    setHidden(newHidden);
+  };
 
-const ProjectsComponent = () => { 
+  const toggleInfo = (index) => {
+    const newProjectStates = [...projectStates];
+    newProjectStates[index] = !newProjectStates[index];
+    setProjectStates(newProjectStates);
+    changeVisibility(index); // Cambia la visibilidad específica de este proyecto
+  };
 
-  return(
-   
-         <section id="projects" className="section__projects">
-               <div className='section__projects__div__title'>
-                   <h1 className='section__projects__div__h1'>Last Projects</h1>
-               </div>
-                      <div className='section__projects__div'>
-                              
-                              {Project.map((pr, index)=>
-                                     <div  key={index} className='section__projects__div__wrapper'>
+  return (
+    <section id="projects" className="section__projects">
+      <div className='section__projects__div__title'>
+        <h1 className='section__projects__div__h1'>Last Projects</h1>
+      </div>
+      <div className='section__projects__div'>
+        {Project.map((pr, index) => (
+          <div key={index} className='section__projects__div__wrapper'>
+            <div className='section__projects__div__wrapper__video'>
+              <video width="100%" height="100%" controls>
+                <source src={pr.video} type='video/mp4' />
+              </video>
+            </div>
+            <div className={`section__projects__div__wrapper__data ${projectStates[index] ? 'hidden' : ''}`}>
+              <h1>{pr.projectName}</h1> <h4>{pr.date}</h4>
+              <div className='section__projects__div__wrapper__skills'>
+                {pr.skills && pr.skills.map((skill, skillIndex) => (
+                  <span key={skillIndex}>
+                    <img className='icons' src={skill.image} alt={skill.name} />
+                  </span>
+                ))}
+              </div>
+              {pr.url && pr.url.map((links) => (
+                <div key={links} className='section__projects__div__wrapper__url'>
+                  <Link className='link' to={links.vercel}><FontAwesomeIcon icon={faLink} /></Link>
+                  <Link className='link' to={links.gitHub}><FontAwesomeIcon icon={faGithub} /></Link>
+                </div>
+              ))}
+            </div>
 
-                                            <div className='section__projects__div__wrapper__video'> 
-                                               <video width="100%" height="100%" controls>
-                                                 <source src={pr.video} type='video/mp4'/>
-                                               </video>
-                                            </div>
-                
-                                           <div className='section__projects__div__wrapper__data'>
-                                           
-                                                 <h1>{pr.projectName}</h1> <h4>{pr.date}</h4> 
-                                                        <div className='section__projects__div__wrapper__skills'>
-                                                             {pr.skills && pr.skills.map((skill, index)=>
-                                                                 <span key={index}>
-                                                                     <img className='icons' src={skill.image} alt={skill.name}/> 
-                                                                 </span>
-                                                             )} 
-                                                        </div>
+            <div className='section__projects__div__wrapper__button'>
+              <button className='more' onClick={() => toggleInfo(index)}>
+                {projectStates[index] ? "-" : "+"}
+              </button>
+            </div>
 
-                                                        {pr.url && pr.url.map((links)=>
-                                                             <div key={links} className='section__projects__div__wrapper__url'> 
-                                                               <Link className='link' to={links.vercel}><FontAwesomeIcon icon={faLink} /></Link>
-                                                               <Link className='link' to={links.gitHub}><FontAwesomeIcon icon={faGithub} /></Link>
-                                                                         <div className='section__projects__div__wrapper__button'>
-                                                                             <button className='more'>+</button>
-                                                                         </div>
-                                                                  </div>
-                                                             )}
-                                             </div>
-                                     </div>
-                                 )}
-                       
-                       </div>
-         </section>
-          )
+             <div className={`div__description ${hidden[index] ? '' : 'hidden'}`}>
+              <p className='description'>{pr.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 ProjectsComponent.propTypes = {};
